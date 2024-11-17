4abab22eb0e766c9dd38aee5998da3ea79d635f8
Letícia Aguiar <letsaguiar@yandex.com>
2024-11-17 02:44:56 -0300

    feat: install undotree

--- lua/core/keybinds.lua
+++ lua/core/keybinds.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>vf", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-n>", "<cmd>Telescope find_files<cr>")
 vim.keymap.set("n", "<C-g>", "<cmd>Telescope live_grep<cr>")
+
+vim.keymap.set("n", "<C-u>", "<cmd>UndotreeToggle<cr>")
